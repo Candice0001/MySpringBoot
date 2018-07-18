769b2d8e2e51058fccd65ea166f1fe5a1486789e
Candice0001 <1986267634@qq.com>
2018-07-18 11:25:57 +0800

    Update testjenkins.py

--- testjenkins.py
+++ testjenkins.py
@@ -4,7 +4,7 @@ import os
 
 def readFile():
     print os.path.join(os.path.dirname(__file__),'test1.txt')
-    with open(r'D:/workspace_python/hello/test1.txt', 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__),'test1.txt'), 'r') as f:
         data = f.read()
         print data
 
